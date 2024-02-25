464386e21501b8993a4fdc4f5cd9286d89016a2f
cyberbiont <cyberbiont@gmail.com>
2024-02-25 20:28:53 +0200

    add solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { PostsListComponent } from './features/posts/posts-list/posts-list.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PostsListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'task';
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule)],
 };
