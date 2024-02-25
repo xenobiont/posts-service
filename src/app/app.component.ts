3f4b1eb9a2ee6c9fed49dbe06c6de933b535a72b
cyberbiont <cyberbiont@gmail.com>
2024-02-25 20:30:21 +0200

    delete solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { PostsListComponent } from './features/posts/posts-list/posts-list.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PostsListComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
